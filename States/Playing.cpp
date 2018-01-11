8a87bac58843bd4061f94b61f4e6aeef980f70eb
Samuel <Sommosgaming@gmail.com>
2018-01-11 18:41:31 +0000

    Create Playing.cpp

---
+++ States/Playing.cpp
@@ -0,0 +1,37 @@
+#include "Playing.h"
+
+#include <iostream>
+
+namespace State
+{
+    Playing::Playing(Application& application)
+    :   Game_State (application)
+    ,   m_model    ({0.5, 0.5,
+                     -0.5, 0.5,
+                     -0.5, -0.5,
+                     -0.5, -0.5,
+                     0.5, -0.5,
+                     0.5, 0.5})
+    {
+
+    }
+
+    void Playing::input()
+    {
+
+    }
+
+    void Playing::update()
+    {
+
+    }
+
+    void Playing::draw()
+    {
+        m_model.bind();
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        m_model.unbind();
+    }
+}
