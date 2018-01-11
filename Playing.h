e5f5c2274515cbbcedbfbf424b208bea2aa29cf4
Samuel <Sommosgaming@gmail.com>
2018-01-11 18:30:18 +0000

    Add files via upload
    Basic program that uses OpenGL in C++ to produce a white oblong.

---
+++ Playing.h
@@ -0,0 +1,26 @@
+#ifndef PLAYING_H_INCLUDED
+#define PLAYING_H_INCLUDED
+
+#include "Game_State.h"
+
+#include "../Model.h"
+
+class Application;
+
+namespace State
+{
+    class Playing : public Game_State
+    {
+        public:
+            Playing(Application& application);
+
+            void input()    override;
+            void update()   override;
+            void draw()     override;
+
+        private:
+            Model m_model;
+    };
+}
+
+#endif // PLAYING_H_INCLUDED
