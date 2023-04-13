31ae21486f1d7ded19c9c03a11aff7c84d593ac4
Samuel <Sommosgaming@gmail.com>
2023-04-13 20:52:13 +0100

    refactored and updated comments within display.cpp

--- Display.cpp
+++ Display.cpp
@@ -5,42 +5,45 @@
 
 namespace Display
 {
+    // width and height variables for the current window
     constexpr static int WIDTH = 1280;
     constexpr static int HEIGHT = 720;
-
+    // pointer for RenderWindow object
     std::unique_ptr<sf::RenderWindow> window;
 
     void init()
     {
+        // bit depth setting and version of OpenGL in use
         sf::ContextSettings settings;
         settings.depthBits = 24;
         settings.majorVersion = 3;
         settings.minorVersion = 3; //OpenGL 3.3
-
+        // render the window at the size variables above
         window = std::make_unique<sf::RenderWindow>(sf::VideoMode(WIDTH, HEIGHT), "Window", sf::Style::Close, settings);
         glewInit();
         glViewport(0, 0, WIDTH, HEIGHT);
-
-
-
     }
-
+    
+    // method that allows the user to close the current display window
     void close()
     {
         window->close();
     }
 
+    // method to clear the current display window and set to a colour
     void clear()
     {
         glClearColor(0.0, 0.0, 0.0, 1.0);
         glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     }
 
+    // method to update the current display window
     void update()
     {
         window->display();
     }
 
+    // method to check if the current window is closed
     void checkForClose()
     {
         sf::Event e;
@@ -53,6 +56,7 @@ namespace Display
         }
     }
 
+    // method to check if the current window is open
     bool isOpen()
     {
         return window->isOpen();
