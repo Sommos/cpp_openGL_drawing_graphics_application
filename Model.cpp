24afeb3de6bf9b8ceeb5ee66167acd09e8be0ac6
Samuel <Sommosgaming@gmail.com>
2023-06-26 17:13:11 +0100

    refactored Model.cpp

--- Model.cpp
+++ Model.cpp
@@ -2,50 +2,56 @@
 
 Model::Model(const std::vector<GLfloat>& vertexPositions)
 {
+    // generate a vertex array object (VAO)
     glGenVertexArrays(1, &m_vao);
-    glBindVertexArray(m_vao);
+    // bind the VAO
+    glBindVertexArray(m_vao); 
+    
+    // add a vertex buffer object (VBO) for the vertex positions
+    addVBO(2, vertexPositions); 
 
-    addVBO(2, vertexPositions);
-
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // unbind the VAO
+    glBindVertexArray(0); 
+    // unbind the buffer
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
 }
 
 Model::~Model()
 {
-    glDeleteVertexArrays(1, &m_vao);
+    // delete the VAO
+    glDeleteVertexArrays(1, &m_vao); 
 
-    glDeleteBuffers(m_buffers.size(), m_buffers.data());
+    // delete the VBOs
+    glDeleteBuffers(m_buffers.size(), m_buffers.data()); 
 }
 
 void Model::bind()
 {
-    glBindVertexArray(m_vao);
+    // bind the VAO for rendering
+    glBindVertexArray(m_vao); 
 }
 
 void Model::unbind()
 {
-    glBindVertexArray(0);
+    // unbind the VAO
+    glBindVertexArray(0); 
 }
 
 void Model::addVBO(int dim, const std::vector<GLfloat>& data)
 {
     GLuint vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER,
-                 data.size() * sizeof(data[0]),
-                 data.data(),
-                 GL_STATIC_DRAW);
+    // generate a VBO
+    glGenBuffers(1, &vbo); 
+    // bind the VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo); 
+    // copy the data to the VBO
+    glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(data[0]), data.data(), GL_STATIC_DRAW); 
 
-    glVertexAttribPointer(m_vboCount,
-                          dim,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          0,
-                          (GLvoid*) 0);
+    // specify the layout of the vertex attribute
+    glVertexAttribPointer(m_vboCount, dim, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0); 
+    // enable the vertex attribute
+    glEnableVertexAttribArray(m_vboCount++); 
 
-    glEnableVertexAttribArray(m_vboCount++);
-
-    m_buffers.push_back(vbo);
+    // store the VBO in the list of buffers
+    m_buffers.push_back(vbo); 
 }
