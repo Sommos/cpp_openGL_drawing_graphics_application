d31fc39c6017028d8eb9f27fc3ce8be26428a5e6
Samuel <Sommosgaming@gmail.com>
2023-06-26 17:06:34 +0100

    refactored Shader_Program.cpp

--- Shaders/Shader_Program.cpp
+++ Shaders/Shader_Program.cpp
@@ -1,27 +1,33 @@
 #include "Shader_Program.h"
-
 #include "Shader_Loader.h"
 
 namespace Shader
 {
+    // constructor that loads and creates a shader program using the provided vertex and fragment shader files
     Shader_Program::Shader_Program(const std::string& vertexShaderFile, const std::string& fragmentShaderFile)
-    :   m_programID(loadShader(vertexShaderFile, fragmentShaderFile))
+        : m_programID(loadShader(vertexShaderFile, fragmentShaderFile))
     {
-
+        // the shader program ID is initialized by calling the loadShader() function from Shader_Loader.h
     }
 
+    // destructor that deletes the shader program
     Shader_Program::~Shader_Program()
     {
-        glDeleteProgram(m_programID);
+        // delete the shader program object
+        glDeleteProgram(m_programID); 
     }
 
+    // binds the shader program for rendering
     void Shader_Program::bind()
     {
-        glUseProgram(m_programID);
+        // use the shader program for rendering
+        glUseProgram(m_programID); 
     }
 
+    // unbinds the shader program
     void Shader_Program::unbind()
     {
-        glUseProgram(0);
+        // stop using any shader program (unbind)
+        glUseProgram(0); 
     }
 }
